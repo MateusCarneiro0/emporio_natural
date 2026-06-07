51f7eedabc1409c322d6439bbc510f64b56f3393
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-06-07 16:14:43 -0300

    Creating product page and partializing the Button

--- src/Components/productsComponents/CardProduct.jsx
+++ src/Components/productsComponents/CardProduct.jsx
@@ -1,18 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./CardProduct.module.css";
+import Button from "../Button";
 
-function CardProduct({ src, title, children, categorias, price,id }) {
-  const navigate = useNavigate()
+function CardProduct({ src, title, children, categorias, price, id }) {
+  const navigate = useNavigate();
 
   return (
-    <div className={styles.card} onClick={() => navigate(`${id}`)}>
+    <div className={styles.card}>
       <img src={src} alt={title} />
       <h3>{title}</h3>
       <p className={styles.description}>{children}</p>
       <p className={styles.price}>
         <strong>{price} R$/Kg</strong>
       </p>
-      <button>Adicione ao carrinho</button>
+      <Button onClick={() => navigate(`${id}`)}>Veja mais</Button>
     </div>
   );
 }
--- src/Components/productsComponents/Product.jsx
+++ src/Components/productsComponents/Product.jsx
@@ -1,27 +1,73 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { getProduct } from "../features/productsSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ThreeDots } from "react-loader-spinner";
 import Error from "../Error";
-
+import styles from "./Product.module.css";
+import Button from "../Button";
 function Product() {
+  const [quantity, setQuantity] = useState(1);
+
   const dispatch = useDispatch();
+
   const { id } = useParams();
+
   const { isLoading, currentProduct, error } = useSelector(
     (store) => store.products,
   );
+  const { nome, imagem, categorias, descricao } = currentProduct;
   useEffect(() => {
     dispatch(getProduct(id));
-  }, [id,dispatch]);
+  }, [id, dispatch]);
 
   if (isLoading) return <ThreeDots />;
 
   if (error) return <Error />;
 
   return (
-    <div>
-      {currentProduct.id}:{currentProduct.nome}
+    <div className={styles.product}>
+      <div className={styles.imgContainer}>
+        <img alt={nome} src={imagem} />
+      </div>
+      <div className={styles.informations}>
+        <h1>{nome}</h1>
+        <p className={styles.description}>{descricao}</p>
+        <p className={styles.categoryContainer}>
+          Categorias:{" "}
+          {categorias?.map((categoria, index) => (
+            <strong key={categoria}>
+              {index === categorias.length - 1
+                ? `${categoria}.`
+                : `${categoria},`}
+            </strong>
+          ))}
+        </p>
+        <div
+          className={styles.priceContainer}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          <input
+            type="number"
+            placeholder="digite uma quantidade"
+            className={styles.productInput}
+            value={quantity || 1}
+            onChange={(ev) => {
+              setQuantity((quantity) =>
+                +ev.target.value < 0 ? quantity : +ev.target.value,
+              );
+            }}
+          />
+          <p className={styles.price}>Total:{25 * quantity} R$</p>
+        </div>
+        <Button>Adicionar ao carrinho</Button>
+      </div>
     </div>
   );
 }
