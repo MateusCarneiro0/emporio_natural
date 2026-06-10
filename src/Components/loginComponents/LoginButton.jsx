839a0a8a9cbec0f22f92bc157325ce37c1eec909
Mateus Henrique <mateushenrique20240512@gmail.com>
2026-06-10 08:02:54 -0300

    Adding disabled prop in loginButton conponent

--- src/Components/loginComponents/LoginButton.jsx
+++ src/Components/loginComponents/LoginButton.jsx
@@ -1,11 +1,12 @@
 import styles from "./LoginButton.module.css";
 
-function LoginButton({ children, color, backgroundColor, onClick }) {
+function LoginButton({ children, color, backgroundColor, onClick,disabled }) {
   return (
     <button
       onClick={onClick}
       className={styles.button}
       style={{ color, backgroundColor }}
+      disabled={disabled}
     >
       {children}
     </button>
