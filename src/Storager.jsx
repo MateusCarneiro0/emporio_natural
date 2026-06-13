bb213588213cd19798a3dfb93599d81dc6697975
Mateus Henrique <mateushenrique20240512@gmail.com>
2026-06-13 18:30:50 -0300

    Adding receiveProducts on storager

--- src/Storager.jsx
+++ src/Storager.jsx
@@ -2,12 +2,16 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import getLocalStorage from "./Components/features/localStorageThunk";
 import { fetchCart } from "./Components/features/cartSlice";
-
+import {receiveProducts} from "./Components/features/productSlice"
 function Storager({ children }) {
   const { isAuthenticated, authUserId: userId } = useSelector(
     (store) => store.auth,
   );
+  const {products} = useSelector(store => store.products)
   const dispatch = useDispatch();
+  useEffect(() => {
+    if(products.length !== 0) dispatch(receiveProducts())
+  },[products])
 
   useEffect(() => {
     if (!isAuthenticated) {
