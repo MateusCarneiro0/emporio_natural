abe31777e3627d5a7843dc48e1e5cf319bc35cce
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-06-30 09:26:23 -0300

    Moving productsSlice to slices folder

--- src/Components/productsComponents/Product.jsx
+++ src/Components/productsComponents/Product.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getProduct } from "../features/productsSlice";
+import { getProduct } from "../../slices/productsSlice";
 
 import Error from "../Error";
 import Button from "../Button";
