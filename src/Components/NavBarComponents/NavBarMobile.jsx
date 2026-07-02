044cb12009c896e651d29374a0ed35af99652cff
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-07-02 16:07:17 -0300

    Creating a prototype of NAvBar mobile

--- src/Components/NavBarComponents/NavBarMobile.jsx
+++ src/Components/NavBarComponents/NavBarMobile.jsx
@@ -3,16 +3,14 @@ import Logo from "./Logo";
 import styles from "./NavBar.module.css";
 import IconButton from "@mui/material/IconButton";
 import WestIcon from "@mui/icons-material/West";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { useSelector } from "react-redux";
-import NavLoginButton from "./NavLoginButton";
 import LogoutIconNav from "./LogoutIconNav";
+import NavModal from "./NavModal";
 
 function NavBarMobile() {
   const { isAuthenticated } = useSelector((store) => store.auth);
   const { id } = useParams();
   const navigate = useNavigate();
-  const { cartProducts } = useSelector((store) => store.cart);
 
   return (
     <nav className={styles.nav}>
@@ -28,33 +26,7 @@ function NavBarMobile() {
       <NavLink className={styles.textLink} to="/">
         <Logo />
       </NavLink>
-      <NavLink to="/produtos" className={styles.textLink}>
-        Produtos
-      </NavLink>
-      <a href="mailto:emporionatural36@gmail.com" className={styles.textLink}>
-        Contato
-      </a>
-      {isAuthenticated ? (
-        <NavLink to="/cart" className={styles.textLink}>
-          {({ isActive }) => (
-            /* O NavLink expõe 'isActive'. Passamos isso para o IconButton ou para o Ícone */
-            <IconButton>
-              <ShoppingCartOutlinedIcon
-                sx={{
-                  // Se estiver ativo, fica azul, se não, fica cinza
-                  color: isActive ? "rgb(170, 173, 121)" : "#757575",
-                  transition: "color 0.2s ease",
-                }}
-              />
-              <span>
-                {!cartProducts?.length ? null : `(${cartProducts?.length})`}
-              </span>
-            </IconButton>
-          )}
-        </NavLink>
-      ) : (
-        <NavLoginButton />
-      )}
+      <NavModal />
     </nav>
   );
 }
---
+++ src/Components/NavBarComponents/NavModal.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import styles from "./NavModal.module.css";
+import { NavLink } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { useSelector } from "react-redux";
+import NavLoginButton from "./NavLoginButton";
+function NavModal() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { isAuthenticated } = useSelector((store) => store.auth);
+
+  const { cartProducts } = useSelector((store) => store.cart);
+  return (
+    <div className={`${styles.modal} ${isOpen ? styles.fixed : ""}`}>
+      {isOpen ? (
+        <button
+          className={styles.menuButton}
+          onClick={() => setIsOpen((isOpened) => !isOpened)}
+        >
+          &times;
+        </button>
+      ) : (
+        <button
+          className={styles.menuButton}
+          onClick={() => setIsOpen((isOpened) => !isOpened)}
+        >
+          &#9776;
+        </button>
+      )}
+
+      {isOpen && (
+        <main className={styles.links}>
+          <div className={styles.linkContainer}>
+            <NavLink to="/produtos" className={styles.textLink}>
+              Produtos
+            </NavLink>
+          </div>
+          <div className={styles.linkContainer}>
+            <a
+              href="mailto:emporionatural36@gmail.com"
+              className={styles.textLink}
+            >
+              Contato
+            </a>
+          </div>
+          <div className={styles.linkContainer}>
+            {isAuthenticated ? (
+              <NavLink to="/cart" className={styles.textLink}>
+                {({ isActive }) => (
+                  /* O NavLink expõe 'isActive'. Passamos isso para o IconButton ou para o Ícone */
+                  <IconButton>
+                    <ShoppingCartOutlinedIcon
+                      sx={{
+                        // Se estiver ativo, fica azul, se não, fica cinza
+                        color: isActive ? "rgb(170, 173, 121)" : "#757575",
+                        transition: "color 0.2s ease",
+                      }}
+                    />
+                    <span>
+                      {!cartProducts?.length
+                        ? null
+                        : `(${cartProducts?.length})`}
+                    </span>
+                  </IconButton>
+                )}
+              </NavLink>
+            ) : (
+              <NavLoginButton />
+            )}
+          </div>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default NavModal;
