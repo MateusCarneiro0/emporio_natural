51db780cde0e68df48dac342430dd8cc5d67fda8
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-07-02 15:26:29 -0300

    Creating NavBar desktop and mobile navbar

--- src/Components/NavBarComponents/NavBar.jsx
+++ src/Components/NavBarComponents/NavBar.jsx
@@ -1,62 +1,8 @@
-import { NavLink, useNavigate, useParams } from "react-router-dom";
-import Logo from "./Logo";
-import styles from "./NavBar.module.css";
-import IconButton from "@mui/material/IconButton";
-import WestIcon from "@mui/icons-material/West";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { useSelector } from "react-redux";
-import NavLoginButton from "./NavLoginButton";
-import LogoutIconNav from "./LogoutIconNav";
+import NavBarMobile from "./NavBarMobile";
+import NavBarDesktop from "./NavBarDesktop";
 
 function NavBar() {
-  const { isAuthenticated } = useSelector((store) => store.auth);
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const { cartProducts } = useSelector((store) => store.cart);
-
-  return (
-    <nav className={styles.nav}>
-      {id ? (
-        <NavLink>
-          <IconButton onClick={() => navigate(-1)}>
-            <WestIcon />
-          </IconButton>
-        </NavLink>
-      ) : (
-        isAuthenticated && <LogoutIconNav />
-      )}
-      <NavLink className={styles.textLink} to="/">
-        <Logo />
-      </NavLink>
-      <NavLink to="/produtos" className={styles.textLink}>
-        Produtos
-      </NavLink>
-      <a href="mailto:emporionatural36@gmail.com" className={styles.textLink}>
-        Contato
-      </a>
-      {isAuthenticated ? (
-        <NavLink to="/cart" className={styles.textLink}>
-          {({ isActive }) => (
-            /* O NavLink expõe 'isActive'. Passamos isso para o IconButton ou para o Ícone */
-            <IconButton>
-              <ShoppingCartOutlinedIcon
-                sx={{
-                  // Se estiver ativo, fica azul, se não, fica cinza
-                  color: isActive ? "rgb(170, 173, 121)" : "#757575",
-                  transition: "color 0.2s ease",
-                }}
-              />
-              <span>
-                {!cartProducts?.length ? null : `(${cartProducts?.length})`}
-              </span>
-            </IconButton>
-          )}
-        </NavLink>
-      ) : (
-        <NavLoginButton />
-      )}
-    </nav>
-  );
+  return <>{window.innerWidth <= 720 ? <NavBarMobile /> : <NavBarDesktop />}</>;
 }
 
 export default NavBar;
