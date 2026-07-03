e14ce7900d484714ba984d67cd8fa895c4b85689
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-07-03 13:21:49 -0300

    Adding some styles to modal concluding the modal part

--- src/Components/NavBarComponents/NavBarMobile.jsx
+++ src/Components/NavBarComponents/NavBarMobile.jsx
@@ -1,32 +1,17 @@
-import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
-import styles from "./NavBar.module.css";
-import IconButton from "@mui/material/IconButton";
-import WestIcon from "@mui/icons-material/West";
-import { useSelector } from "react-redux";
-import LogoutIconNav from "./LogoutIconNav";
+import styles from "./NavBarMobile.module.css";
 import NavModal from "./NavModal";
 
 function NavBarMobile() {
-  const { isAuthenticated } = useSelector((store) => store.auth);
-  const { id } = useParams();
-  const navigate = useNavigate();
-
   return (
     <nav className={styles.nav}>
-      {id ? (
-        <NavLink>
-          <IconButton onClick={() => navigate(-1)}>
-            <WestIcon />
-          </IconButton>
-        </NavLink>
-      ) : (
-        isAuthenticated && <LogoutIconNav />
-      )}
-      <NavLink className={styles.textLink} to="/">
-        <Logo />
-      </NavLink>
       <NavModal />
+      <div>
+        <NavLink className={styles.textLink} to="/">
+          <Logo />
+        </NavLink>
+      </div>
     </nav>
   );
 }
--- src/Components/NavBarComponents/NavModal.jsx
+++ src/Components/NavBarComponents/NavModal.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import styles from "./NavModal.module.css";
-import { NavLink } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { useSelector } from "react-redux";
 import NavLoginButton from "./NavLoginButton";
+import LogoutIconNav from "./LogoutIconNav";
+import { NavLink, useParams, useNavigate } from "react-router-dom";
+import WestIcon from "@mui/icons-material/West";
+
 function NavModal() {
   const [isOpen, setIsOpen] = useState(false);
   const { isAuthenticated } = useSelector((store) => store.auth);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
   const { cartProducts } = useSelector((store) => store.cart);
   return (
     <div className={`${styles.modal} ${isOpen ? styles.fixed : ""}`}>
       {isOpen ? (
         <button
-          className={styles.menuButton}
+          className={`${styles.menuButton} ${styles.closeButton}`}
           onClick={() => setIsOpen((isOpened) => !isOpened)}
         >
           &times;
@@ -66,6 +71,17 @@ function NavModal() {
               </NavLink>
             ) : (
               <NavLoginButton />
+            )}
+          </div>
+          <div className={styles.linkContainer}>
+            {id ? (
+              <NavLink>
+                <IconButton onClick={() => navigate(-1)}>
+                  <WestIcon />
+                </IconButton>
+              </NavLink>
+            ) : (
+              isAuthenticated && <LogoutIconNav />
             )}
           </div>
         </main>
