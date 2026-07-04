15227ff64eaa2c85f2b5c7293cd83989b0e558c8
Mateus Henrique <mateushenrique20240512@gmail.com>
2026-07-04 13:10:48 -0300

    Adding a button to return to home page

--- src/Components/notFoundComponents/NotFoundMain.jsx
+++ src/Components/notFoundComponents/NotFoundMain.jsx
@@ -1,12 +1,14 @@
 import styles from "./NotFoundMain.module.css";
-
+import {useNavigate} from "react-router-dom"
 function NotFound() {
+  const navigate = useNavigate()
   return (
     <div className={styles.notFoundContainer}>
       <h1 className={styles.code}>404</h1>
       <h3 className={styles.message}>
         A rota especificada não existe tente outra rota
       </h3>
+      <button className={styles.button} onClick={() => navigate("/")}>Voltar</button>
     </div>
   );
 }
