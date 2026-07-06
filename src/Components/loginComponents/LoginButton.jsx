2491ff0fd4ef5bfd4d0163d20984a6ebf358a879
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-07-06 19:50:40 -0300

    removing overflow-x global

--- src/Components/loginComponents/LoginButton.jsx
+++ src/Components/loginComponents/LoginButton.jsx
@@ -3,7 +3,7 @@ import styles from "./LoginButton.module.css";
 function LoginButton({ children, disabled, register }) {
   return (
     <button
-      aria-label={register ? "registrar-se" : "Entrar"}
+      aria-label={register ? "Registrar-se" : "Entrar"}
       type="submit"
       className={`${styles.button} ${register ? styles.register : styles.login}`}
       disabled={disabled}
