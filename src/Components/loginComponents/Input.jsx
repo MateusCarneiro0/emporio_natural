54a22ca7e1a0476c5ad728fa2b223acc25014166
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-07-07 16:23:25 -0300

    Creating name and id in loginInput

--- src/Components/loginComponents/Input.jsx
+++ src/Components/loginComponents/Input.jsx
@@ -1,10 +1,17 @@
 import styles from "./Input.module.css";
 
 function Input({ setState, placeholder, value, disabled, type, signup }) {
+  const name = `${type}-${signup ? "register" : "password"}`;
   return (
     <input
+      name={name}
+      id={name}
       autoComplete={
-        type === "user" ? "username" : signup ? "new-password" : "current-password"
+        type === "user"
+          ? "username"
+          : signup
+            ? "new-password"
+            : "current-password"
       }
       type={type}
       className={styles.input}
