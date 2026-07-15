542488aa98f2d38b9fc864dcf6a96907ed838ce8
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-07-15 10:26:29 -0300

    Adding a new error message

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -3,6 +3,7 @@ import styles from "./Error.module.css";
 function Error({ message }) {
   return (
     <div className={styles.errorContainer}>
+      <h2 className={styles.message}>Ocorreu um erro 😭</h2>
       <h3 className={styles.errorMessage}>
         {message ? message : "Um erro ocorreu tente denovo"}
       </h3>
