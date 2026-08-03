7c3aaa8c6d14b4ff64501dfbfacbcfaedfa74f30
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-08-03 20:29:52 -0300

    Creating a Error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import LoggedProtected from "./Components/protectedRoutes/LoggedProtected";
 import AuthProtected from "./Components/protectedRoutes/AuthProtected";
 import SpinnerFullScreen from "./Components/SpinnerFullScreen";
 import AppLayout from "./Components/AppLayout";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "./Components/Error";
 
 const Home = lazy(() => import("./Pages/Home"));
 const Products = lazy(() => import("./Pages/Products"));
@@ -16,48 +18,50 @@ const NotFound = lazy(() => import("./Pages/NotFound"));
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<SpinnerFullScreen />}>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/produtos" element={<Products />}>
+      <ErrorBoundary fallback={<Error />}>
+        <Suspense fallback={<SpinnerFullScreen />}>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/produtos" element={<Products />}>
+                <Route
+                  path="/produtos/:id"
+                  element={
+                    <AuthProtected>
+                      <Product />
+                    </AuthProtected>
+                  }
+                />
+              </Route>
               <Route
-                path="/produtos/:id"
+                path="/cart"
                 element={
                   <AuthProtected>
-                    <Product />
+                    <Cart />
                   </AuthProtected>
                 }
               />
+              <Route
+                path="/login"
+                element={
+                  <LoggedProtected>
+                    <Login />
+                  </LoggedProtected>
+                }
+              />
+              <Route
+                path="/signup"
+                element={
+                  <LoggedProtected>
+                    <Signup />
+                  </LoggedProtected>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
             </Route>
-            <Route
-              path="/cart"
-              element={
-                <AuthProtected>
-                  <Cart />
-                </AuthProtected>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <LoggedProtected>
-                  <Login />
-                </LoggedProtected>
-              }
-            />
-            <Route
-              path="/signup"
-              element={
-                <LoggedProtected>
-                  <Signup />
-                </LoggedProtected>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
