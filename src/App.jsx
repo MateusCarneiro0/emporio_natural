7a6bbd093ae6ac0c8cfa129a0a186a633bef93a2
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-08-09 10:06:14 -0300

    Refactoring to the v7

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+} from "react-router";
 import { lazy, Suspense } from "react";
 import LoggedProtected from "./Components/protectedRoutes/LoggedProtected";
 import AuthProtected from "./Components/protectedRoutes/AuthProtected";
@@ -7,6 +12,7 @@ import AppLayout from "./Components/AppLayout";
 import { ErrorBoundary } from "react-error-boundary";
 import Error from "./Components/Error";
 import AppBootstrap from "./AppBootstrap";
+import { RouterProvider } from "react-router/dom";
 
 const Home = lazy(() => import("./Pages/Home"));
 const Products = lazy(() => import("./Pages/Products"));
@@ -15,57 +21,69 @@ const Product = lazy(() => import("./Components/productsComponents/Product"));
 const Login = lazy(() => import("./Pages/Login"));
 const Signup = lazy(() => import("./Pages/Signup"));
 const NotFound = lazy(() => import("./Pages/NotFound"));
-
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        element: <Home />,
+        path: "/",
+      },
+      {
+        element: <Products />,
+        path: "/produtos",
+        children: [
+          {
+            element: (
+              <AuthProtected>
+                <Product />
+              </AuthProtected>
+            ),
+            path: "/produtos/:id",
+          },
+        ],
+      },
+      {
+        element: (
+          <AuthProtected>
+            <Cart />
+          </AuthProtected>
+        ),
+        path: "/cart",
+      },
+      {
+        element: (
+          <LoggedProtected>
+            <Login />
+          </LoggedProtected>
+        ),
+        path: "/login",
+      },
+      {
+        element: (
+          <LoggedProtected>
+            <Signup />
+          </LoggedProtected>
+        ),
+        path: "/signup",
+      },
+      {
+        element: <NotFound />,
+        path: "*",
+      },
+    ],
+  },
+]);
 function App() {
   return (
-    <BrowserRouter>
+    
       <ErrorBoundary fallback={<Error />}>
         <AppBootstrap>
           <Suspense fallback={<SpinnerFullScreen />}>
-            <Routes>
-              <Route element={<AppLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/produtos" element={<Products />}>
-                  <Route
-                    path="/produtos/:id"
-                    element={
-                      <AuthProtected>
-                        <Product />
-                      </AuthProtected>
-                    }
-                  />
-                </Route>
-                <Route
-                  path="/cart"
-                  element={
-                    <AuthProtected>
-                      <Cart />
-                    </AuthProtected>
-                  }
-                />
-                <Route
-                  path="/login"
-                  element={
-                    <LoggedProtected>
-                      <Login />
-                    </LoggedProtected>
-                  }
-                />
-                <Route
-                  path="/signup"
-                  element={
-                    <LoggedProtected>
-                      <Signup />
-                    </LoggedProtected>
-                  }
-                />
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
+            <RouterProvider router={router} />
           </Suspense>
         </AppBootstrap>
       </ErrorBoundary>
-    </BrowserRouter>
   );
 }
 
