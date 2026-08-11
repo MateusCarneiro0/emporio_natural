f71c465d626a41a83ca61d27eef413624691893a
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-08-11 17:11:28 -0300

    Creating ToastAlert

--- src/Components/cartComponents/CartMain.jsx
+++ src/Components/cartComponents/CartMain.jsx
@@ -15,7 +15,7 @@ function CartMain() {
   const dispatch = useDispatch();
 
   const totalCust = Number(
-    (cartProducts?.length 
+    (cartProducts?.length
       ? cartProducts.reduce((prev, cur) => prev + (cur?.total || 0), 0)
       : 0
     ).toFixed(2),
@@ -34,26 +34,28 @@ function CartMain() {
       </div>
     );
   return (
-    <div className={styles.cart}>
-      {cartProducts?.map((product) => (
-        <CartCard
-          key={product.id}
-          src={product.imagem}
-          price={product.total}
-          productId={product.id}
-          quantity={product.quantity}
-          categoria={product.categoria}
-        >
-          {product.nome}
-        </CartCard>
-      ))}
-      <hr style={{ marginTop: "15px", marginBottom: "15px" }} />
-      <h2 style={{ color: "rgb(192, 195, 137)" }}>
-        Total:<strong>{totalCust} R$</strong>
-      </h2>
-      <Button onClick={() => dispatch(payCart())}>Pagar o carrinho</Button>
+    <>
+      <div className={styles.cart}>
+        {cartProducts?.map((product) => (
+          <CartCard
+            key={product.id}
+            src={product.imagem}
+            price={product.total}
+            productId={product.id}
+            quantity={product.quantity}
+            categoria={product.categoria}
+          >
+            {product.nome}
+          </CartCard>
+        ))}
+        <hr style={{ marginTop: "15px", marginBottom: "15px" }} />
+        <h2 style={{ color: "rgb(192, 195, 137)" }}>
+          Total:<strong>{totalCust} R$</strong>
+        </h2>
+        <Button onClick={() => dispatch(payCart())}>Pagar o carrinho</Button>
+      </div>
       <ToastAlert />
-    </div>
+    </>
   );
 }
 
--- src/Components/cartComponents/ToastAlert.jsx
+++ src/Components/cartComponents/ToastAlert.jsx
@@ -1,11 +1,28 @@
-import styles from './ToastAlert.module.css'
-
-function ToastAlert() {
-    return (
-        <div className={styles.container}>
-            HELLO
-        </div>
-    )
+import styles from "./ToastAlert.module.css";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+function ToastAlert({
+  error = true,
+  text = "A operação de adicionar produto Banana Prata foi bem-sucedida",
+}) {
+  return (
+    <div
+      className={`${styles.container} ${error ? styles.wrong : styles.correct}`}
+    >
+      <div className={styles.toast}>
+        {error ? (
+          <CancelOutlinedIcon
+            sx={{ fill: "red", width: "25px", height: "25px" }}
+          />
+        ) : (
+          <CheckCircleOutlineRoundedIcon
+            sx={{ fill: "greenyellow", width: "25px", height: "25px" }}
+          />
+        )}
+        <p className={styles.message}>{text}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ToastAlert
+export default ToastAlert;
