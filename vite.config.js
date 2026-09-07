8e1fe27242ce96abd75ed3ed1dea54db53b7f53d
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-09-07 14:41:07 -0300

    Putting the header in server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; connect-src 'self' https://onrender.com; style-src 'self' 'unsafe-inline' https://googleapis.com; font-src 'self' https://gstatic.com;",
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
