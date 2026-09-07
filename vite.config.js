75920634da9e350f25c992dd4a32a9fac3c70f77
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-09-07 14:05:11 -0300

    REmoving the failOnError on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-//Use in .vite.config.js 6 version
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({
-      failOnError: false, // REMOVER DEPOIS
-      failOnWarning: false,
-    })],
+  plugins: [react(), eslint()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.js'], 
-  }
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.js"],
+  },
 });
